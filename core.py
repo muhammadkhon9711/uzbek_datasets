b9336040adc622069c3f522f0453853d4818567b
Fiery <fiery.dev@outlook.com>
2023-07-24 10:38:22 +0500

    MySQL added

--- core.py
+++ core.py
@@ -1,50 +1,182 @@
 import aiofiles as aiof
 import pickle
 import aiosqlite as aiosql
+import aiomysql as aiomysql
 import python_translator as pytr
 import datasets as dts
 
 datasets_path = "./datasets"
-database = "./datasets/database/sentences.db"
+database = "./datasets/database/sentences_joined.db"
+
+async def create_mysql_tables():
+    print("create_mysql_tables ...")
+    commands = [
+        "drop database if exists sentences_nlp;",
+        "create database if not exists sentences_nlp;",
+        "use sentences_nlp",
+        "drop table if exists en_ru_sentences;",
+        "drop table if exists en_tr_sentences;",
+        "drop table if exists en_uz_sentences;",
+        """create table if not exists en_ru_sentences (
+        id integer primary key auto_increment,
+        dest text,
+        src text,
+        tr text
+      );""",
+        """create table if not exists en_tr_sentences (
+        id integer primary key auto_increment,
+        dest text,
+        src text,
+        tr text
+      );""",
+        """create table if not exists en_uz_sentences (
+        id integer primary key auto_increment,
+        dest text,
+        src text,
+        tr text
+      );"""
+    ]
+
+    host, user, password, _, port = mysql_config()
+    async with await aiomysql.connect(host, user, password, port=port) as db:
+        curr = await db.cursor()
+        for sql in commands:
+            await curr.execute(sql)
+        await db.commit()
+    print("create_mysql_tables done.")
 
 async def create_tables():
     print("create_table ...")
     commands = [
-      "drop table if exists en_ru_sentences;",
-      "drop table if exists en_tr_sentences;",
-      "drop table if exists en_uz_sentences;",
-      """create table if not exists en_ru_sentences (
+        "drop table if exists en_ru_sentences;",
+        "drop table if exists en_tr_sentences;",
+        "drop table if exists en_uz_sentences;",
+        """create table if not exists en_ru_sentences (
         id integer primary key autoincrement,
         dest text,
         src text,
         tr text
       );""",
-      """create table if not exists en_tr_sentences (
+        """create table if not exists en_tr_sentences (
         id integer primary key autoincrement,
         dest text,
         src text,
         tr text
       );""",
-      """create table if not exists en_uz_sentences (
+        """create table if not exists en_uz_sentences (
         id integer primary key autoincrement,
         dest text,
         src text,
         tr text
       );"""
     ]
-    async with aiosql.connect(database) as db:
+    async with await connect() as db:
         for sql in commands:
             await db.execute(sql)
         await db.commit()
     print("create_table done.")
 
+def mysql_config():
+    host = "localhost"
+    user = "root"
+    password = "9711"
+    database = "sentences_nlp"
+    port = 3306
+    return (host, user, password, database, port)
+
+async def connect_mysql(loop):
+    host, user, password, database, port = mysql_config()
+    return await aiomysql.create_pool(host=host, user=user, password=password, db=database, port=port, loop=loop)
+
+async def close_mysql(connpool):
+    connpool.close()
+    await connpool.wait_closed()
+
+
 async def connect():
     return await aiosql.connect(database)
+
+async def pickle2mydbf(curr, dataset, tablename, dest, src):
+    print(f"pickle2mydbf {tablename}({dataset['count']}) ...")
+
+    #escape = str.maketrans({"'": "''", '"': '""'})
+
+    dest_list = dataset[dest]
+    src_list = dataset[src]
+    size = dataset["count"]
+    try:
+        dss = ""
+        srs = ""
+        for i in range(size):
+            ds = dest_list[i]#.translate(escape)
+            sr = src_list[i]#.translate(escape)
+            if len(srs + sr) >= 5000:
+                command = f"insert ignore into {tablename}(dest, src) value('{dss}', '{srs}');"
+                await curr.execute(command)
+                dss = ""
+                srs = ""
+            dss += ds + "\n"
+            srs += sr + "\n"
+    except aiomysql.Error as ex:
+        print(ex)
+        print(ex.args)
+        print("---------------------------------")
+        print(dest_list[i], src_list[i])
+        print(tablename, "------------------------------")
+    print(f"pickle2db {tablename}({dataset['count']}) done.")
+
+async def pickle2dbf(db, dataset, tablename, dest, src):
+    print(f"pickle2mydbf {tablename}({dataset['count']}) ...")
+
+    escape = str.maketrans({"'": "''", '"': '""'})
+
+    dest_list = dataset[dest]
+    src_list = dataset[src]
+    size = dataset["count"]
+    try:
+        dss = ""
+        srs = ""
+        for i in range(size):
+            ds = dest_list[i].translate(escape)
+            sr = src_list[i].translate(escape)
+            if len(srs + sr) >= 5000:
+                command = f"insert or ignore into {tablename}(dest, src) values('{dss}', '{srs}');"
+                await db.execute(command)
+                dss = ""
+                srs = ""
+            dss += ds + "\n"
+            srs += sr + "\n"
+    except aiosql.Error as ex:
+        print(ex)
+        print(ex.args)
+        print(dest_list[i], src_list[i])
+    print(f"pickle2db {tablename}({dataset['count']}) done.")
+
+async def pickle2mydb(db:aiomysql.Connection, dataset, tablename, dest, src):
+    print(f"pickle2mydb {tablename}({dataset['count']}) ...")
+
+    escape = str.maketrans({"'": "''", '"': '""'})
+    curr = db.cursor()
+    dest_list = dataset[dest]
+    src_list = dataset[src]
+    size = dataset["count"]
+    try:
+        for i in range(size):
+            ds = dest_list[i].translate(escape)
+            sr = src_list[i].translate(escape)
+            command = f"insert or ignore into {tablename}(dest, src) values('{ds}', '{sr}');"
+            await curr.execute(command)
+
+    except aiomysql.Error as ex:
+        print(ex)
+        print(ex.args)
+        print(dest_list[i], src_list[i])
+    print(f"pickle2mydb {tablename}({dataset['count']}) done.")
 
 async def pickle2db(db, dataset, tablename, dest, src):
     print(f"pickle2db {tablename}({dataset['count']}) ...")
 
-    escape = str.maketrans({"'":"''", '"':'""'})
+    escape = str.maketrans({"'": "''", '"': '""'})
 
     dest_list = dataset[dest]
     src_list = dataset[src]
@@ -55,11 +187,23 @@ async def pickle2db(db, dataset, tablename, dest, src):
             sr = src_list[i].translate(escape)
             command = f"insert or ignore into {tablename}(dest, src) values('{ds}', '{sr}');"
             await db.execute(command)
+
     except aiosql.Error as ex:
         print(ex)
         print(ex.args)
         print(dest_list[i], src_list[i])
     print(f"pickle2db {tablename}({dataset['count']}) done.")
+
+
+async def text2english(dataset, output, count):
+    print(f"text2english {output}({dataset['count']})...")
+    path = f"{datasets_path}/translated"
+    size = count
+    async with aiof.open(f"{path}/{output}-en.txt", "a") as file:
+        for i in range(size):
+            text = dataset["en"][i]
+            await file.write(text + "\n")
+    print(f"text2english {output} done.")
 
 async def translate2text(dataset, output, language_from, language_to):
     print(f"translate2text {output}({dataset['count']})...")
@@ -70,7 +214,8 @@ async def translate2text(dataset, output, language_from, language_to):
         for i in range(size):
             text_from = dataset[language_from][i]
             try:
-                tr = str(translator.translate(text=text_from, source_language=language_from, target_language=language_to))
+                tr = str(translator.translate(
+                    text=text_from, source_language=language_from, target_language=language_to))
                 if tr == None or tr == "":
                     await file_logs.write(f"{i},")
                     continue
@@ -79,15 +224,13 @@ async def translate2text(dataset, output, language_from, language_to):
             await file_from.write(text_from + "\n")
             await file_to.write(tr + "\n")
     print(f"translate2text {output} done.")
-            
-
-
 
 def make_dataset(languages):
     return {
         "languages": languages,
         "count": 0
     }
+
 
 async def load_pickle(filename):
     async with aiof.open(f"{datasets_path}/pickle/{filename}.pickle", "rb") as file:
@@ -101,7 +244,7 @@ async def save_pickle(filename, dataset):
         await file.write(data)
 
 
-async def dataset2pickle(name:str, translation:str, outputfile:str):
+async def dataset2pickle(name: str, translation: str, outputfile: str):
     print(f"dataset2pickle {name}({translation})...")
     data = dts.load_dataset(name, translation)
     languages = translation.split("-")
@@ -114,7 +257,8 @@ async def dataset2pickle(name:str, translation:str, outputfile:str):
         for sentence in data[branch]:
             count += 1
             for language in languages:
-                text = str(sentence['translation'][language]).replace("\n", " ")
+                text = str(sentence['translation']
+                           [language]).replace("\n", " ")
                 dataset[language].append(text)
     dataset["count"] = count
     await save_pickle(outputfile, dataset)
--- database_run.py
+++ database_run.py
@@ -3,8 +3,15 @@ import asyncio, core
 async def insert(db, file, table, dest, src):
     print(f"{file} prossesing...")
     dataset = await core.load_pickle(file)
-    await core.pickle2db(db, dataset, table, dest, src) 
+    await core.pickle2dbf(db, dataset, table, dest, src) 
     print(f"{file} done.")
+
+async def insert_mysql(curr, file, table, dest, src):
+    print(f"{file} prossesing...")
+    dataset = await core.load_pickle(file)
+    await core.pickle2mydbf(curr, dataset, table, dest, src) 
+    print(f"{file} done.")
+
 
 async def main():
     await core.create_tables()
@@ -27,4 +34,28 @@ async def main():
     await db.commit()
     await db.close()
 
-asyncio.run(main())
+async def main_mysql(loop:asyncio.AbstractEventLoop):
+    await core.create_mysql_tables()
+    files = [
+        ("opus_infopankki-en-ru", "en_ru_sentences", "en", "ru"), # -
+        ("opus_wikipedia-en-ru", "en_ru_sentences", "en", "ru"), # -
+        ("opus100-en-ru", "en_ru_sentences", "en", "ru"), # -
+        ("wmt16-en-ru", "en_ru_sentences", "en", "ru"), # +
+        ("opus_infopankki-en-tr", "en_tr_sentences", "en", "tr"),
+        ("opus100-en-tr", "en_tr_sentences", "en", "tr"),
+        ("opus100-en-uz", "en_uz_sentences", "en", "uz"),
+        ("wmt16-en-tr", "en_tr_sentences", "en", "tr"),
+    ]
+    tasks = []
+    db = await core.connect_mysql()
+    curr = await db.cursor()
+    for i in files:
+        tasks.append(asyncio.create_task(insert_mysql(curr, i[0], i[1], i[2], i[3])))
+    await asyncio.wait(tasks)
+    await db.commit()
+    await db.close()
+
+#asyncio.run(core.create_mysql_tables())
+loop = asyncio.get_event_loop()
+asyncio.run(main_mysql(loop))
+#asyncio.run(main())
