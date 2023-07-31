086fc345e7e0de275df3a8740b27ac8ac2e39468
Fiery <fiery.dev@outlook.com>
2023-07-31 21:54:05 +0500

    Release 0.0.1

--- checktinydb.py
+++ checktinydb.py
@@ -2,11 +2,15 @@ from tinydb import TinyDB, Query
 from tinydb.middlewares import CachingMiddleware
 from tinydb.storages import JSONStorage
 from tinydb_smartcache import SmartCacheTable
+import re
 
 # TODO: o'zizga moslang
 def __dbpath(file):
     return f"./datasets/database/{file}.json"
 
+
+def __textpath(file):
+    return f"./datasets/text/{file}.txt"
 
 # ! TEGMANG
 def connect(name: str) -> TinyDB:
@@ -18,6 +22,33 @@ def close(db: TinyDB):
         return
     db.close()
     db = None
+
+def db2text(datasetname, databasename):
+    db = connect(databasename)
+    db.table_class = SmartCacheTable
+    try:
+        # !TODO agar ma'lumotlarni o'chirib yuborish kerak bo'lsa
+        # db.drop_table(datasetname)
+        table = db.table(datasetname)
+        length = len(table)
+        q = Query()
+        r = table.search(q.i == length-1)
+        print(length)
+        s = ""
+        print(r[0]["text"][0:200])
+        print(r[0]["tr"])
+    except Exception as ex:
+        print("ERROR")
+        print(ex)
+        print(ex.with_traceback())
+        close(db)
+        return
+    except KeyboardInterrupt:
+        print("program stopped")
+        close(db)
+        return
+    finally:
+        close(db)
 
 def check(datasetname, database=None):
     print("proccessing ...")
@@ -32,9 +63,22 @@ def check(datasetname, database=None):
         # db.drop_table(datasetname)
         table = db.table(datasetname)
         length = len(table)
-        q = Query()
-        r = table.search(q.i == 100)
-        print(length, len(r[0]["tr"]))
+        print(length)
+        with open(__textpath(f"{datasetname}-en"), "w") as fen, open(__textpath(f"{datasetname}-uz"), "w") as fuz:
+            for r in table.all():
+                ens = r["en"].split("\n")
+                uzs = r["uz"].split("\n")
+                for en in ens:
+                    en = en.strip()
+                    if len(en) <= 1:
+                        continue
+                    fen.write(en + "\n")
+                for uz in uzs:
+                    uz = uz.strip()
+                    if len(uz) <= 1:
+                        continue
+                    fuz.write(uz + "\n")
+            
     except Exception as ex:
         print("ERROR")
         print(ex)
@@ -48,4 +92,5 @@ def check(datasetname, database=None):
     finally:
         close(db)
 
-check("wikipedia-list-20", "wikidb")
+check("wikipedia-list-1", "wikidb-1")
+print("done.")
--- dataset_translater.py
+++ dataset_translater.py
@@ -5,6 +5,8 @@ from tinydb import TinyDB, Query
 from tinydb.middlewares import CachingMiddleware
 from tinydb.storages import JSONStorage
 from tinydb_smartcache import SmartCacheTable
+import time
+import re
 
 # ! shu paketlarni yuklash kerak
 # * pip install tinydb tinydb_smartcache googletrans==3.1.0a0
@@ -45,9 +47,48 @@ def load_pickle(filename: str):
     return raw
 
 # * Bu yerda berilgan matnni kichik qismlarga ajratadi
+def qcointer(text):
+    qc = 0
+    rc = 0
+    ec = 0
+    gc = 0
+    for c in text:
+        if c == "(": 
+            qc += 1
+            continue
+        if c == ")":
+            qc -= 1
+            continue
+        if c == "[":
+            rc += 1
+            continue
+        if c == "]":
+            rc -= 1
+        if c == "{":
+            ec += 1
+            continue
+        if c == "}":
+            ec -= 1
+            continue
+        if c == "\"":
+            gc += 1
+    return qc == 0 and rc == 0 and ec == 0 and (gc % 2 == 0)
 
+def _split_dot(text:str) -> str:
+    texts = text.split(".")
+    new = ""
+    dots = []
+    for t in texts:
+        t = t.strip()
+        if len(t) == 0:
+            continue
+        new += t + "."
+        if qcointer(new) == True:
+            dots.append(new)
+            new = ""
+    return "\n".join(dots)
 
-def _split(text: str) -> list:
+def _split_enter(text: str) -> list:
     texts = text.split("\n")
     new = ""
     news = []
@@ -55,12 +96,14 @@ def _split(text: str) -> list:
         if len(new + t) > 4999:
             news.append(new)
             new = ""
-        new += t
+        new += t + "\n"
+    news.append(new)
     return news
+
 
 # * Tarjima qilish logikasi
 
-
+exp = re.compile(r"\n{1,}")
 def translate(text):
     translator = gtr.Translator()
     # * XATOLIK MATNI
@@ -69,13 +112,13 @@ def translate(text):
     # * Agar matn 5000 ko'p bo'lsa maydalab tarjima qilib yana juftlaydi
     if len(text) > 5000:
         trs = []
-        for partial_text in _split(text):
+        for partial_text in _split_enter(text):
             tr = translator.translate(
                 text=partial_text, src="en", dest="uz").text
             if tr is None:
                 tr = error
             trs.append(tr)
-        return "\n ".join(trs)
+        return "\n".join(trs)
     tr = translator.translate(text=text, src="en", dest="uz").text
     if tr is None:
         tr = error
@@ -100,21 +143,21 @@ def main(datasetname, database=None):
         table = db.table(datasetname)
         start = len(table)
         for i in range(start, end):
-            text = dataset[i]
+            text = _split_dot(re.sub(exp, ".", dataset[i]))
             translated = translate(text)
-            table.insert({"text": text, "tr": translated, "i": i})
+            table.insert({"en": text, "uz": translated, "i": i})
     except Exception as ex:
         print("ERROR")
         print(ex)
-        print(ex.with_traceback())
         close(db)
-        return
+        return -1
     except KeyboardInterrupt:
         print("program stopped")
         close(db)
-        return
+        return 0
     finally:
         close(db)
+    return 0
 
 
 def check(datasetname, database=None):
@@ -148,10 +191,36 @@ def check(datasetname, database=None):
 
 
 # * Ctrl + C da xavfsiz dastur ishni yakunlaydi
-# source ./.venv/bin/activate
-# python dataset_translater.py wikipedia-list-20 wikidb-20
+"""
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-1 wikidb-1
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-2 wikidb-2
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-3 wikidb-3
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-4 wikidb-4
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-5 wikidb-5
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-6 wikidb-6
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-7 wikidb-7
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-8 wikidb-8
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-9 wikidb-9
+
+source ./.venv/bin/activate && python dataset_translater.py wikipedia-list-10 wikidb-10
+"""
 print("Use Ctrl + C for safe stop program")
 datasetname = sys.argv[1]
 dbname = sys.argv[2]
-main(datasetname, dbname)
+code = 1
+try: 
+    while (code != 0):
+        code = main(datasetname, dbname)
+        print("program returned code:", code, " wait 5 seconds")
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("Program stopped")
 print("all done.")
